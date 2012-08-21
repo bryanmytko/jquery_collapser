a135d4eb6a86cb88d97f4aea09f7f65ad1feae16
Bryan Mytko <bryanmytko@gmail.com>
2012-08-21 11:55:35 -0400

    clean up, fixed for IE

--- js/collapser.js
+++ js/collapser.js
@@ -8,31 +8,29 @@ Author: bryanmytko@gmail.com
 ****************************/
 (function($){
 	var $_b;
+	var $_u;
 	var $_class;
-	$.fn.Collapser = function(a){
-		var $_b = $(this);
-		/* prevents a css bug when containers elements have padding */
-    $_b.next('div').css({'display': 'none','overflow' : 'hidden'});
+	$.fn.Collapser = function(a){	
+		$_b = $(this);	
+		/* hides elements & prevents a css bug when containers elements have padding */
+    $_b.next('div').css({'display': 'none','overflow' : 'hidden'});   
 		/* sets 'open' class */
-		$_class = (a.openClass) ? a.openClass : 'cOpen';
+		$_class = (a.openClass) ? a.openClass : 'cOpen';		
 		/* checks for units set to be open on load */
 		$_b.each(function(){
-			if($(this).hasClass($_class))
-				$(this).next('div').show();
-		});
-	  //$_b.hasClass($_class) ? console.log('open') : console.log('closed');
+			if($(this).hasClass($_class)) $(this).next('div').show();
+		});		
 	  $_b.click(function(){
 			$(this).toggleClass($_class);
-		  var $_u = $(this).next();
-			if(a.animate) animateToggle($_u);
-			else $_u.toggle();
-		});
-		function animateToggle(u){
-			if(a.fade) u.slideFadeToggle(a.speed);
-			else u.stop(true, true).slideToggle(a.speed);
-		}
+		  $_u = $(this).next();
+			(a.animate) ? animateToggle($_u) : $_u.toggle();
+		});	
 	}
+	function animateToggle(u){
+		(a.fade) ? u.slideFadeToggle(a.speed) : u.stop(true, true).slideToggle(a.speed);
+	}	
+	/* Slide Toggle effect */
 	$.fn.slideFadeToggle = function(speed) {
 	  return this.animate({opacity: 'toggle', height: 'toggle'}, speed);
-	};
+	};	
 })(jQuery);
